3da6394d9716b21b0a56c82f7de35a91d8de5f5a
XorsDomain <110067216+XorsDomain@users.noreply.github.com>
2026-07-24 23:39:56 -0400

    Duplicated a test scene and created player controller

---
+++ Assets/zzz_Workspaces/XorsDomain/Scripts/Gameplay/PlayerController.cs
@@ -0,0 +1,233 @@
+using UnityEngine;
+
+public enum CharacterType
+{
+    Firefighter,
+    RiotOfficer,
+    RescueSpecialist
+}
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Current Character")]
+    public CharacterType activeCharacter;
+
+    [Header("Movement")]
+    public float moveSpeed = 5f;
+    public float climbSpeed = 3f;
+    public float crawlSpeed = 2f;
+
+    [Header("State")]
+    public bool isPaused;
+    public bool onLadder;
+    public bool inWater;
+    public bool inConfinedSpace;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPaused)
+        {
+            PauseControls();
+            return;
+        }
+
+        GameplayControls();
+    }
+
+    private void GameplayControls()
+    {
+        HandleMovement();
+
+        if (Input.GetKeyDown(KeyCode.F))
+            UseSelectedItem();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            Activate();
+
+        if (Input.GetKeyDown(KeyCode.J))
+            Ability1();
+
+        if (Input.GetKeyDown(KeyCode.K))
+            Ability2();
+
+        if (Input.GetKeyDown(KeyCode.Q))
+            PreviousCharacter();
+
+        if (Input.GetKeyDown(KeyCode.E))
+            NextCharacter();
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+            PauseGame();
+    }
+
+    private void HandleMovement()
+    {
+        // Horizontal movement
+        float horizontal = 0;
+
+        if (Input.GetKey(KeyCode.A))
+            horizontal = -1;
+
+        if (Input.GetKey(KeyCode.D))
+            horizontal = 1;
+
+        transform.Translate(Vector3.right * horizontal * moveSpeed * Time.deltaTime);
+
+        // W only climbs
+        if (Input.GetKey(KeyCode.W) && onLadder)
+        {
+            transform.Translate(Vector3.up * climbSpeed * Time.deltaTime);
+        }
+
+        // S climbs down or crawls
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (onLadder)
+            {
+                transform.Translate(Vector3.down * climbSpeed * Time.deltaTime);
+            }
+            else if (activeCharacter == CharacterType.RescueSpecialist &&
+                     inConfinedSpace)
+            {
+                Crawl();
+            }
+        }
+    }
+
+    private void Ability1()
+    {
+        switch (activeCharacter)
+        {
+            case CharacterType.Firefighter:
+                ToggleLadder();
+                break;
+
+            case CharacterType.RiotOfficer:
+                ToggleShield();
+                break;
+
+            case CharacterType.RescueSpecialist:
+                Jump();
+                break;
+        }
+    }
+
+    private void Ability2()
+    {
+        switch (activeCharacter)
+        {
+            case CharacterType.Firefighter:
+                AxeAttack();
+                break;
+
+            case CharacterType.RiotOfficer:
+                StunBaton();
+                break;
+
+            case CharacterType.RescueSpecialist:
+                JetSwim();
+                break;
+        }
+    }
+
+    private void PauseControls()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+            ResumeGame();
+
+        if (Input.GetKeyDown(KeyCode.Return))
+            OpenQuitConfirmation();
+    }
+
+    #region Gameplay Actions
+
+    void UseSelectedItem()
+    {
+        Debug.Log("Use Item");
+    }
+
+    void Activate()
+    {
+        Debug.Log("Activate");
+    }
+
+    void PreviousCharacter()
+    {
+        Debug.Log("Previous Character");
+    }
+
+    void NextCharacter()
+    {
+        Debug.Log("Next Character");
+    }
+
+    void PauseGame()
+    {
+        isPaused = true;
+        Debug.Log("Paused");
+    }
+
+    void ResumeGame()
+    {
+        isPaused = false;
+        Debug.Log("Resumed");
+    }
+
+    void OpenQuitConfirmation()
+    {
+        Debug.Log("Quit Confirmation");
+    }
+
+    #endregion
+
+    #region Character Abilities
+
+    void ToggleLadder()
+    {
+        Debug.Log("Place / Retrieve Ladder");
+    }
+
+    void AxeAttack()
+    {
+        Debug.Log("Axe Attack");
+    }
+
+    void ToggleShield()
+    {
+        Debug.Log("Shield Toggle");
+    }
+
+    void StunBaton()
+    {
+        Debug.Log("Stun Baton");
+    }
+
+    void Jump()
+    {
+        if (!onLadder)
+            Debug.Log("Jump");
+
+        if (inWater)
+            Debug.Log("Swim Up");
+    }
+
+    void JetSwim()
+    {
+        if (inWater)
+            Debug.Log("Jet Swim");
+    }
+
+    void Crawl()
+    {
+        transform.Translate(Vector3.right * crawlSpeed * Time.deltaTime);
+    }
+
+    #endregion
+}
