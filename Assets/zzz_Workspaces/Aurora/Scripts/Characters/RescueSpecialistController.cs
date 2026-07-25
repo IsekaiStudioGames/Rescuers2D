67f3397fa2caab0725b81bf254f393cf2de313a0
JesseAdams313 <echodevgames@gmail.com>
2026-07-25 05:48:08 -0400

    Riot and Rescue idle and walk
    adding anims to riot and rescue pushing to get a fresh pull and ill add to firefighter im prob gonna refactor this out into an object i can calll a hel[er from

--- Assets/zzz_Workspaces/Aurora/Scripts/Characters/RescueSpecialistController.cs
+++ Assets/zzz_Workspaces/Aurora/Scripts/Characters/RescueSpecialistController.cs
@@ -6,12 +6,37 @@ public class RescueSpecialistController : MonoBehaviour {
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float moveSpeed = 5f;
 
+    private Animator animator;
+
+
+
     private void Start() {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
     public void Move(Vector2 moveDirection) {
 
         rb.MovePosition(rb.position + (moveDirection * moveSpeed) * Time.fixedDeltaTime);
+
+        // Face left/right
+        if (moveDirection.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (moveDirection.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        //walk anim
+        if (moveDirection != Vector2.zero)
+        {
+            animator.SetBool("IsMoving", true);
+
+        }
+        else
+        {
+            animator.SetBool("IsMoving", false);
+        }
     }
     internal void Crawl() { throw new NotImplementedException(); }
     internal void Jump() {
